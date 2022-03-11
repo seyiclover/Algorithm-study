3837596f0e5218e6b38688da325c9d7aecc0c7ee
seyiclover <92769643+seyiclover@users.noreply.github.com>
2022-03-11 22:24:15 +0900

    Update 03-10_ex03.py
    [문제 설명]
    도시의 아파트에서 버스 정류장까지 거리를 구하려고 합니다. 도시는 격자 모양의 지역으로 구분되어 있으며 아파트는 1로, 버스 정류장은 0으로 표시되어 있습니다. 아파트에서 버스 정류장으로 이동은 상하좌우로만 이동할 수 있으며, 대각선으로는 이동이 불가합니다.
    h x w 크기의 도시가 주어질 때, 각 아파트에서 가장 가까운 버스 정류장까지의 거리를 구하는 함수, solution을 완성해주세요.
    
    예를 들어, 3 x 3 크기의 도시[[1, 0, 1], [1, 1, 1], [1, 1, 1]]가 주어질 때, 각 아파트에서 가까운 버스 정류장까지의 거리를 나타내는 결과는 [[ 1, 0, 1 ], [ 2, 1, 2 ], [ 3, 2, 3 ]] 입니다.
    
    [제한 사항]
    
    도시에서 이동은 상하좌우로만 이동할 수 있으며, 대각선으로는 이동할 수 없습니다.
    도시에서 버스 정류장은 적어도 하나 이상 존재합니다.
    도시에는 아파트와 버스 정류장만 존재합니다.
    [입력 형식]
    
    도시의 정보 city가 주어집니다.
    city는 0과 1로 이루어진 h x w 크기의 2차원 배열입니다.
    도시의 크기 h와 w는 1 이상 50 이하의 정수입니다.
    [출력 형식]
    
    아파트에서 버스 정류장까지의 최단 거리를 출력합니다.
    결과는 h x w 크기의 2차원 배열입니다.

--- Zerobase_codingtest/03-10_ex03.py
+++ Zerobase_codingtest/03-10_ex03.py
@@ -1,4 +1,50 @@
-def solution(city):
-    for each in city:
-        if 0 in each:
+import numpy
 
+
+def solution(city):
+    """
+    :param city: int[][]
+    :return: int[][]
+    """
+    return calc_distance(city)
+
+
+def calc_distance(city):
+    apt_locations = []
+    bus_locations = []
+
+    get_locations(city, apt_locations, bus_locations)
+
+    result = create_base_result(city)
+
+    for apt_index in range(len(apt_locations)):
+        apt_y = apt_locations[apt_index][0]
+        apt_x = apt_locations[apt_index][1]
+        for bus_index in range(len(bus_locations)):
+            distance = get_distance(apt_locations[apt_index], bus_locations[bus_index])
+            result[apt_y][apt_x] = distance if (result[apt_y][apt_x] == 0) else min(result[apt_y][apt_x], distance)
+
+    return result
+
+
+def get_locations(city, apt_locations, bus_locations):
+    y_size = len(city)
+    x_size = len(city[0])
+
+    for y in range(y_size):
+        for x in range(x_size):
+            if city[y][x] == 1:
+                apt_locations.append([y, x])
+            else:
+                bus_locations.append([y, x])
+
+
+def create_base_result(city):
+    y_size = len(city)
+    x_size = len(city[0])
+
+    return numpy.zeros(shape=(y_size, x_size), dtype=object).tolist()
+
+
+def get_distance(apt_location, bus_location):
+    return abs(apt_location[0] - bus_location[0]) + abs(apt_location[1] - bus_location[1])
