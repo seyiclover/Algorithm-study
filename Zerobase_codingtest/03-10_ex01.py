2e45bce5a35a3c772d89540050e172d794e6ae33
seyiclover <92769643+seyiclover@users.noreply.github.com>
2022-03-15 16:43:44 +0900

    Update 03-10_ex01.py
    n개의 문자를 보여주는 크기가 n인 전광판이 있습니다. 전광판의 문자는 오른쪽에서 왼쪽으로 반복해서 흘러가며, 1초에 한 글자씩 흘러갑니다.
    
    예를 들어, 크기가 5인 전광판에 "Snowball" 노출한다고 가정할 때, 시간 t의 변화에 따른 노출 예시는 다음과 같습니다.
    
    t : 0초
    전광판 : .....
    
    t : 1초
    전광판 : ....S
    
    t : 5초
    전광판 : Snowb
    
    t : 10초
    전광판 : all..
    
    t : 15초
    전광판 : ...Sn
    
    전광판의 크기 n과 전광판에 노출할 문자 s 그리고 시간 t가 주어질 때, t 초 후의 전광판에 표시될 문구를 출력하는 함수, solution을 완성해주세요.

--- Zerobase_codingtest/03-10_ex01.py
+++ Zerobase_codingtest/03-10_ex01.py
@@ -1,22 +1,10 @@
+# 전광판의 크기(*)와 노출할 글자를 합쳐서 전체 글자로 보고 어디서부터 어디까지 인덱싱할 것인지로 단순화하는 것이 핵심 아이디어
 def solution(n, s, t):
-    newS = list(s)
-    if t < n:
-        result = '.' * (n-t) + newS[:t]
+    # 노출할 글자 전체(*가 앞뒤로 노출될 것이므로 입력)
+    word = ("." * n) + s + ('.' * (n-1))
+    # 인덱싱 시작 지점
+    t = t % len(word)
+
+    result = word[t:t+n]
 
     return result
-
-# 정답코드
-def solution(n, s, t):
-    '''
-    :param n: number
-    :param s: string
-    :param t: number
-    :return: string
-    '''
-    repeat_duration = n + len(s)
-
-    optimize_time = t % repeat_duration
-
-    text = ('.' * n) + s + ('.' * (n - 1))
-    return text[optimize_time:optimize_time + n]
-
