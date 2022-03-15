7d5584131c592f82e711ab569d8f0aa150511ccf
seyiclover <92769643+seyiclover@users.noreply.github.com>
2022-03-15 16:52:40 +0900

    Update 03-10_ex02.py
    [문제 설명]
    블록으로 피라미드 모양의 탑을 쌓으려고 합니다. 피라미드 모양의 탑은 꼭대기는 1개의 블록을 사용하고, 그 아래는 2개의 블록, 그 아래에는 3개의 블록의 방식으로 쌓습니다.
    n 층의 피라미드 모양의 탑을 쌓을 때, 필요한 블록의 수를 구하는 함수, solution을 완성해주세요.
    
    예를 들어, 4층의 피라미드에는 총 10개의 블록이 필요합니다.
    
    4층에 사용되는 블록 수 1개
    3층에 사용되는 블록 수 2개
    2층에 사용되는 블록 수 3개
    1층에 사용되는 블록 수 4개
    피라미드에 사용되는 블록의 총수는 10개입니다.
    
    [입력 형식]
    
    n은 1 이상 1,000 이하의 정수입니다.
    [출력 형식]
    
    피라미드 모양의 탑을 쌓는데 필요한 블록의 총수를 int 형식으로 출력합니다.

--- Zerobase_codingtest/03-10_ex02.py
+++ Zerobase_codingtest/03-10_ex02.py
@@ -7,10 +7,9 @@ def solution(n):
     return cnt
 
 # 정답코드
+# 공차가 1인 등차 수열에 해당한다는 점에 착안해 등차수열의 일반항과 합공식을 사용 
+# 일반항: a1(n-1) + d
+# 합공식: (a1 + an) * n/2
 def solution(n):
-    """
-    :param n: int
-    :return: int
-    """
 
     return (1 + n) * n / 2
