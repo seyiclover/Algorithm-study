abe0546816460ee6a023b85e14e81d6253d51712
seyiclover <92769643+seyiclover@users.noreply.github.com>
2022-03-15 19:35:07 +0900

    Update 3-10_ex05.py
    [문제 설명]
    문자열 계산식을 입력받는 계산기를 만들려고 합니다. 계산식은 덧셈(+), 뺄셈(-), 숫자로만 이루어져 있습니다.
    계산식 s가 주어질 때, 계산식의 계산 결과를 출력하는 함수, solution을 완성해주세요.
    
    예를 들어, s가 -3+26-7로 주어질 때, 계산식의 계산 결과는 16 (-3 + 26 -7)입니다.
    
    [제한 사항]
    
    계산식은 덧셈(+), 뺄셈(-), 숫자로만 이루어져 있습니다.
    [입력 형식]
    
    s는 길이가 1 이상 10 이하의 문자열입니다.
    [출력 형식]
    
    계산식의 계산 결과를 int 형식으로 출력합니다.

--- Zerobase_codingtest/3-10_ex05.py
+++ Zerobase_codingtest/3-10_ex05.py
@@ -1,3 +1,32 @@
+# 문자열에서 숫자를 구분해주는 함수 isdigit() 사용
+def solution(s):
+    # 숫자 다음에 +, -  연산자를 인식 시키기 위해서 문자열을 거꾸로 조회한다. 
+    z = s[::-1]
+    total = 0
+    calcu = 0
+    multi = 1
+    for each in z:
+        if each.isnumeric():
+            # 연속되는 숫자에서 자릿수를 인식해주기 위해 10을 곱해준다. 
+            calcu += int(each)*multi
+            multi *= 10
+        else:
+            if each == "-":
+                total -= calcu
+            else:
+                total += calcu
+            # 변수 초기화 
+            calcu = 0
+            multi = 1
+    # 첫번 째 숫자에 연산자가 없을 경우 더해준다.
+    if s[0].isnumeric():
+        total += calcu
+    return int(total)
+
+print(solution('-3+36-7'))
+
+
+<정답코드>
 def solution(s):
     """
     :param s: str
