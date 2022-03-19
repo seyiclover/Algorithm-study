7c2f8eb06a66c6f19e6575472b93d6f83cd05c19
seyiclover <92769643+seyiclover@users.noreply.github.com>
2022-03-19 19:18:25 +0900

    Update 03-17_ex03.py
    문제
    진구는 친구들과 제로섬 게임을 하려고 한다.
    
    제로섬 게임은 N개의 서로 다른 정수로 이루어진 리스트가 주어졌을 때, 더 적은 개수의 수를 더해서 0으로 만들면 이기는 게임이다.
    
    진구는 친구들이 찾은 답이 최적의 답인지 검증하기 위해, 몇 개의 정수를 더해야 하는지 구하는 프로그램을 만들고자 한다.
    
    입력 설명
    길이가 1000 이하인 정수로 이루어진 리스트
    
    출력 설명
    총 합을 0으로 만드는 최소의 정수 개수
    
    단, 답이 5보다 크거나 없는 경우 -1
    
    입력 예시
    [-5, -2, 4, 5, 7]
    
    출력 예시
    2

--- Zerobase_codingtest/03-17/03-17_ex03.py
+++ Zerobase_codingtest/03-17/03-17_ex03.py
@@ -1,12 +1,15 @@
 <정답 코드>
+# itertools 모듈의 combinations 함수 기능: combinations(n, r) => n에서 r개를 뽑아서 나열하는 모든 경우 생성 
+
 from itertools import combinations
 
 def solution(nums):
     n = len(nums)
-    for i in range(1, 5):
-        for indices in combinations(range(n), i):
+    # 답이 5보다 크면 -1을 출력하기 때문에 합을 0으로 만드는 정수 개수의 최대값은 5개이다. 
+    for i in range(1, 6):  # nCr 에서 r 지정 
+        for indices in combinations(range(n), i):   # nCi 의 모든 경우 나열 
             summation = 0
-            for index in indices:
+            for index in indices:                   # i개 뽑은 인덱스를 넣었을 때 그 합이 0인 경우를 찾는다. 
                 summation += nums[index]
             if summation == 0:
                 return i 
