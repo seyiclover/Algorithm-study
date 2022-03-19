03f8266f2a14ee05ffc7868e59eea14c5ff03c1b
seyiclover <92769643+seyiclover@users.noreply.github.com>
2022-03-19 16:46:12 +0900

    Update 03-17_ex01.py
    두 문자열 s1와 s2가 있을 때, 두 문자열의 '최대공약문자열' s3를 아래와 같이 정의하자.
    
    문자열 s3를 반복하여 문자열 s1와 s2를 생성할 수 있다.
    가능한 s3 중에 가장 긴 문자열을 s3로 한다.
    위 조건을 만족하는 s3가 없으면 빈 문자열을 s3로 한다.
    이 때, 문자열 s1와 s2를 입력받아 s3를 출력하는 프로그램을 작성하시오.
    
    입력설명
    매개변수 s1과 s2가 주어지며, len(s1)과 len(s2)는 최대 100000이다.
    
    출력설명
    가능한 s3 중 가장 긴 s3를 반환하시오.
    
    매개변수 형식
    s1 = 'abababab'
    s2 = 'abab'
    
    반환값 형식
    'abab'

--- Zerobase_codingtest/03-17/03-17_ex01.py
+++ Zerobase_codingtest/03-17/03-17_ex01.py
@@ -1,3 +1,7 @@
+# s1이 긴 문자열 s2가 짧거나 길이가 같은 문자열 이라고 정의
+# s2의 약수에 해당하는 문자열을 구한다. 
+# s2의 각 약수를 곱해서 s1을 만들 수 있는 지 확인하고 가장 긴 약수에 해당하는 문자열을 출력한다. 
+
 <내 코드>
 def solution(s1, s2):
 
@@ -8,6 +12,7 @@ def solution(s1, s2):
     for i in range(n):
         for j in range(i+1):
             token = s2[j:n-i+j]
+           
             if token in s1:
                 return token
     else:
@@ -18,14 +23,17 @@ def solution(s1, s2):
 def solution(s1, s2):
   def isDivisor(string, divisor):
     n = len(divisor)
+    # 최대공약문자열이 존재하려면 두 문자열의 길이가 최대공약수를 가져야 한다. 
     if len(string) % n != 0:
       return False
+    # 나누는 문자열의 길이만큼 긴 문자열을 슬라이싱 했을 때 동일하다면 True를 반환하게 된다. 
     while string != '':
       if string[:n] != divisor:
         return False
-      string = string[n:]
+      string = string[n:]              # 문자열의 없는 인덱스를 지정해서 슬라이싱하면 공백 문자가 된다. => ''로 출력됨 
     return True
- 
+
+  # 길이가 긴 문자열을 str1, 길이가 짧거나 같은 문자열을 str2로 지정 
   if len(s1) > len(s2):
     str1, str2 = s1, s2
   else:
@@ -34,7 +42,7 @@ def solution(s1, s2):
   divisor = str2
   m = 1
   while divisor != '':
-    if isDivisor(str2, divisor) and isDivisor(str1, divisor):
+    if isDivisor(str2, divisor) and isDivisor(str1, divisor): 
       return divisor
     m += 1
     divisor = str2[:len(str2) // m]
